24b43e1a8273b43e7e23b0a1af7bf7e17d0accf5
vanmelon <hms2006@online.no>
2024-04-30 10:11:43 +0200

    lager nav bar

--- index.php
+++ index.php
@@ -7,6 +7,26 @@
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+  <header>
+    <nav>
+      <div class="navbar">
+        <div class="nav-left">
+          <div class="nav-box"><a href="#" class="nav-link">Lokasjoner</a></div>
+          <div class="nav-box"><a href="#" class="nav-link">About Us</a></div>
+        </div>
+        <div class="nav-right">
+          <div class="nav-box"><a href="#" class="nav-link">Logg Inn</a></div>
+          <div class="nav-box"><a href="#" class="nav-link">Bli Medlem</a></div>
+        </div>
+      </div>
+    </nav>
+  </header>
 
+  <main>
+    <!-- Her kan du legge til innholdet ditt -->
+  </main>
+
+  <footer>
+  </footer>
 </body>
 </html>
