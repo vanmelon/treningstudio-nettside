19956946e6ea50c5d67d295828f49097f7348a3b
vanmelon <hms2006@online.no>
2024-05-03 11:41:09 +0200

    bugfiksing

--- Bli-Medlem.php
+++ Bli-Medlem.php
@@ -67,7 +67,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Kobler til databasen
-$conn = new mysqli($servername, $username, $password, $dbname);
+$mysqli = new mysqli("localhost","my_user","my_password","my_db");
 
 // Sjekk tilkoblingen
 if ($conn->connect_error) {
